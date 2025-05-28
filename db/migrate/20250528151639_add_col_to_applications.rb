dba7f4ec7cbe607e98b4e97ec3bfffb3297e73d7
sujit <sujitt258@gmail.com>
2025-05-29 01:19:54 +0530

    first change

---
+++ db/migrate/20250528151639_add_col_to_applications.rb
@@ -0,0 +1,7 @@
+class AddColToApplications < ActiveRecord::Migration[8.0]
+  def change
+    add_column :applications, :email, :string
+    add_column :applications, :contact_number, :integer
+    add_column :applications, :address, :string
+  end
+end
