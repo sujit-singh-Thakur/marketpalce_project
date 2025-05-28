788ae72b8f2bea1db3b8f2bb66baf1290c0cef23
sujit <sujitt258@gmail.com>
2025-05-29 01:18:28 +0530

    first change

---
+++ app/controllers/admin/tasks_controller.rb
@@ -0,0 +1,35 @@
+class Admin::TasksController < ApplicationController
+     before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.includes(:user, :category)
+    # @tasks = Task.all
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to admin_tasks_path, notice: "Task updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to admin_tasks_path, notice: "Task deleted."
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.expect(:task [:description, :contact_info, :category_id])
+  end
+end
