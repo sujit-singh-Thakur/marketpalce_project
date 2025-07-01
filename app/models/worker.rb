6b6fc918d0f1c0c10f9e9d05b472b8d9e6b99e9c
sujit thakur <sujitt258@gmail.com>
2025-07-01 18:51:22 +0530

    puma_update

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,6 +1,4 @@
 class Worker < User
   has_many :applications, foreign_key: :worker_id, dependent: :destroy
-
-  # validates :contact,  format: { with: /\A\d{10}\z/, message: " must be 10 digit " }
-  # validates :contact , uniqueness:{message: "has already exist"}
+  
 end
